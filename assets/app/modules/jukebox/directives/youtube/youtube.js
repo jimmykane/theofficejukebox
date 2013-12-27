ca88df9e1582908f2d6f1258ac76bdea7dbada82
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2013-12-27 18:33:27 +0100

    Change identation

--- assets/app/modules/jukebox/directives/youtube/youtube.js
+++ assets/app/modules/jukebox/directives/youtube/youtube.js
@@ -6,169 +6,169 @@
 "use strict";
 
 angular.module('mainApp.jukebox').directive('youtubePlayer', function($window, ui, jukebox_service, player_service) {
-	return {
-		restrict: 'A', // only activate on element attribute
-		scope: true, // New scope to use but rest inherit proto from parent
-		compile: function(tElement, tAttrs) {
-			// Load the Yotube js api
-			var tag = document.createElement('script');
-			tag.src = "https://www.youtube.com/iframe_api";
-			var firstScriptTag = document.getElementsByTagName('script')[0];
-			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-		},
-		controller: function($scope, $element, $attrs) {
+    return {
+        restrict: 'A', // only activate on element attribute
+        scope: true, // New scope to use but rest inherit proto from parent
+        compile: function(tElement, tAttrs) {
+            // Load the Yotube js api
+            var tag = document.createElement('script');
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        },
+        controller: function($scope, $element, $attrs) {
 
-			// This is called when the player is loaded from YT
-			$window.onYouTubeIframeAPIReady = function() {
-				$scope.player = new YT.Player('player', {
-					height: '250',
-					width: '400',
-					playerVars: {
-						'autoplay': 0,
-						'controls': 1,
-						'autohide': 2
-					},
-					//videoId: $scope.live_track.video_id,
-					events: {
-						'onReady': $scope.onPlayerReady,
-						'onStateChange': $scope.onPlayerStateChange,
-						'onError': $scope.onError
-					}
-				});
-			};
+            // This is called when the player is loaded from YT
+            $window.onYouTubeIframeAPIReady = function() {
+                $scope.player = new YT.Player('player', {
+                    height: '250',
+                    width: '400',
+                    playerVars: {
+                        'autoplay': 0,
+                        'controls': 1,
+                        'autohide': 2
+                    },
+                    //videoId: $scope.live_track.video_id,
+                    events: {
+                        'onReady': $scope.onPlayerReady,
+                        'onStateChange': $scope.onPlayerStateChange,
+                        'onError': $scope.onError
+                    }
+                });
+            };
 
-			// When the player has been loaded and is ready to play etc
-			$scope.onPlayerReady = function (event) {
-				$scope.$apply(function(){
-					console.log("Playa is ready");
-					console.log($scope.player);
-					// Lets also broadcast a change state for the others to catch up
-					player_service.broadcast_change_state({"state": $scope.player.getPlayerState()});
-					// Should try to just load the track so that the users can press play on the playa
-				});
-			};
+            // When the player has been loaded and is ready to play etc
+            $scope.onPlayerReady = function (event) {
+                $scope.$apply(function(){
+                    console.log("Playa is ready");
+                    console.log($scope.player);
+                    // Lets also broadcast a change state for the others to catch up
+                    player_service.broadcast_change_state({"state": $scope.player.getPlayerState()});
+                    // Should try to just load the track so that the users can press play on the playa
+                });
+            };
 
-			// When the player changes a state
-			$scope.onPlayerStateChange = function(event) {
-				$scope.$apply(function(){
-					console.log("Playa changed state");
-					// unstarted
-					if ($scope.player.getPlayerState() === -1){
-						player_service.broadcast_change_state({
-							"state": -1,
-							"current_time": $scope.player.getCurrentTime()
-						});
-					}
-					// ended
-					if ($scope.player.getPlayerState() === 0){
-						player_service.broadcast_change_state({
-							"state": 0,
-							"current_time": $scope.player.getCurrentTime()
-						});
-					}
-					// playing
-					if ($scope.player.getPlayerState() === 1){
-						player_service.broadcast_change_state({
-							"state": 1,
-							"current_time": $scope.player.getCurrentTime()
-						});
-					}
-					// paused
-					if ($scope.player.getPlayerState() === 2){
-						player_service.broadcast_change_state({
-							"state": 2,
-							"current_time": $scope.player.getCurrentTime()
-						});
-					}
-					// buffering
-					if ($scope.player.getPlayerState() === 3){
-						player_service.broadcast_change_state({
-							"state": 3,
-							"current_time": $scope.player.getCurrentTime()
-						});
-					}
-					// video cued
-					if ($scope.player.getPlayerState() === 5){
-						player_service.broadcast_change_state({
-							"state": 5,
-							"current_time": $scope.player.getCurrentTime()
-						});
-					}
-				});
-			};
+            // When the player changes a state
+            $scope.onPlayerStateChange = function(event) {
+                $scope.$apply(function(){
+                    console.log("Playa changed state");
+                    // unstarted
+                    if ($scope.player.getPlayerState() === -1){
+                        player_service.broadcast_change_state({
+                            "state": -1,
+                            "current_time": $scope.player.getCurrentTime()
+                        });
+                    }
+                    // ended
+                    if ($scope.player.getPlayerState() === 0){
+                        player_service.broadcast_change_state({
+                            "state": 0,
+                            "current_time": $scope.player.getCurrentTime()
+                        });
+                    }
+                    // playing
+                    if ($scope.player.getPlayerState() === 1){
+                        player_service.broadcast_change_state({
+                            "state": 1,
+                            "current_time": $scope.player.getCurrentTime()
+                        });
+                    }
+                    // paused
+                    if ($scope.player.getPlayerState() === 2){
+                        player_service.broadcast_change_state({
+                            "state": 2,
+                            "current_time": $scope.player.getCurrentTime()
+                        });
+                    }
+                    // buffering
+                    if ($scope.player.getPlayerState() === 3){
+                        player_service.broadcast_change_state({
+                            "state": 3,
+                            "current_time": $scope.player.getCurrentTime()
+                        });
+                    }
+                    // video cued
+                    if ($scope.player.getPlayerState() === 5){
+                        player_service.broadcast_change_state({
+                            "state": 5,
+                            "current_time": $scope.player.getCurrentTime()
+                        });
+                    }
+                });
+            };
 
-			// When the player has been loaded and is ready to play etc
-			$scope.onError = function (event) {
-				$scope.$apply(function(){
-					console.log("Playa Encountered and ERROR");
-					console.log(event)
-				});
-			};
+            // When the player has been loaded and is ready to play etc
+            $scope.onError = function (event) {
+                $scope.$apply(function(){
+                    console.log("Playa Encountered and ERROR");
+                    console.log(event)
+                });
+            };
 
-			$scope.start_playing = function (jukebox_id){
-				console.log('Yes I am starting...');
-				jukebox_service.get_playing_track_async(jukebox_id).then(
-					function(status) {
-						if (status.code === 200) {
-							$scope.player.cueVideoById({
-								'videoId': jukebox_service.get_track_playing().id,
-								'suggestedQuality': 'default'
-							});
-							$scope.player.seekTo(jukebox_service.get_track_playing().start_seconds);
-							//console.log($scope.player)
-						}else if (status.code === 403) {
-							ui.show_notification_warning('Server says: "' + status.message
-							+ '" I asked the backend about the reason and replied: "' + status.info +'"');
-						}else if (status.code === 404) {
-							ui.show_notification_warning('The server did not respond with a playing track. Should I play something from the previous things? ')
-						}else{
-							ui.show_notification_error('Uknown error');
-						}
-						return;
-					},
-					function(status){
-						console.log('The server encountered an errror');
-						return;
-					}
-				);
-			};
+            $scope.start_playing = function (jukebox_id){
+                console.log('Yes I am starting...');
+                jukebox_service.get_playing_track_async(jukebox_id).then(
+                    function(status) {
+                        if (status.code === 200) {
+                            $scope.player.cueVideoById({
+                                'videoId': jukebox_service.get_track_playing().id,
+                                'suggestedQuality': 'default'
+                            });
+                            $scope.player.seekTo(jukebox_service.get_track_playing().start_seconds);
+                            //console.log($scope.player)
+                        }else if (status.code === 403) {
+                            ui.show_notification_warning('Server says: "' + status.message
+                            + '" I asked the backend about the reason and replied: "' + status.info +'"');
+                        }else if (status.code === 404) {
+                            ui.show_notification_warning('The server did not respond with a playing track. Should I play something from the previous things? ')
+                        }else{
+                            ui.show_notification_error('Uknown error');
+                        }
+                        return;
+                    },
+                    function(status){
+                        console.log('The server encountered an errror');
+                        return;
+                    }
+                );
+            };
 
-			$scope.player_status =  function (){
-				try {
-					var status = $scope.player.getPlayerState();
-					//console.log(status);
-					return status;
-				}
-				catch (e) {
-					// probably this happens a lot due to several apply digest cirlces
-					// console.log(e); // pass exception object to error handler
-					return false;
-				}
-			};
+            $scope.player_status =  function (){
+                try {
+                    var status = $scope.player.getPlayerState();
+                    //console.log(status);
+                    return status;
+                }
+                catch (e) {
+                    // probably this happens a lot due to several apply digest cirlces
+                    // console.log(e); // pass exception object to error handler
+                    return false;
+                }
+            };
 
-			$scope.$on('handleStartPlaying', function(event, jukebox_id) {
-				console.log('Got the message I ll play');
-				$scope.start_playing(jukebox_id);
-			});
+            $scope.$on('handleStartPlaying', function(event, jukebox_id) {
+                console.log('Got the message I ll play');
+                $scope.start_playing(jukebox_id);
+            });
 
-			$scope.$on('handlePausePlaying', function() {
-				console.log('Got the message I ll pause');
-				$scope.player.pauseVideo();
-			});
+            $scope.$on('handlePausePlaying', function() {
+                console.log('Got the message I ll pause');
+                $scope.player.pauseVideo();
+            });
 
-			$scope.$on('handleResumePlaying', function() {
-				console.log('Got the message I ll resume');
-				$scope.player.playVideo();
-			});
+            $scope.$on('handleResumePlaying', function() {
+                console.log('Got the message I ll resume');
+                $scope.player.playVideo();
+            });
 
-			$scope.$on('handleStopPlaying', function() {
-				console.log('Got the message I ll stop');
-				$scope.player.stopVideo();
-			});
+            $scope.$on('handleStopPlaying', function() {
+                console.log('Got the message I ll stop');
+                $scope.player.stopVideo();
+            });
 
-		},
-		link: function(scope, elm, attrs, ctrl) {
+        },
+        link: function(scope, elm, attrs, ctrl) {
 
-		}
-	}
+        }
+    }
 });
