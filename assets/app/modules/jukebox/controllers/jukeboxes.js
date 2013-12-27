84405c453b7ad6f8e523ad37b7fdb8d377a4fafb
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2013-12-27 17:45:40 +0100

    Ready to add jukebox controller logic

---
+++ assets/app/modules/jukebox/controllers/jukeboxes.js
@@ -0,0 +1,10 @@
+/*
+ * @author: Dimitrios Kanellopoulos
+ * @contact: jimmykane9@gmail.com
+ */
+
+"use strict";
+
+angular.module('mainApp.jukebox').controller('jukeboxes_controller', function($scope, $location, $routeParams, users_service, jukebox_service, ui, logging, player_service) {
+
+});
